563840001225c67291e9467115ce2299a2b7e996
Hora-keven <kevensantos993@gmail.com>
2025-03-13 12:57:08 -0300

    build

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
      ],
   theme: {
-    extend: {},
+    extend: {
+      
+       
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        grotesk: ["Space Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
