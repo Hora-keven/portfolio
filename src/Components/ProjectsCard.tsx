f81c89e35152efc31c3be4d377337cd99030e1d4
Hora-keven <kevensantos993@gmail.com>
2025-03-13 12:55:12 -0300

    style: css

--- src/Components/ProjectsCard.tsx
+++ src/Components/ProjectsCard.tsx
@@ -4,27 +4,29 @@ export default function ProjectsCard({ image, title, content, rigth = true, link
     return (
 
         rigth ? (
-            <div className=" w-full flex flex-row justify-between p-2">
-                <div className="w-[35rem]">
-                    <img src={image} alt="" />
-                </div>
-                <div className="w-[50%] flex justify-center flex-col gap-3">
-                    <h3 className="font-bold">{title}</h3>
-                    <p className="text-sm">{content}</p>
-                    <a href={link}><span><SiGithub size={30} /></span></a>
-                </div>
+            <div className="w-full flex flex-col md:flex-row gap-4 justify-between p-2">
+            <div className="w-full md:w-[97rem]">
+                <img src={image} alt="" />
             </div>
+            <div className="flex justify-center flex-col gap-3">
+                <h3 className="font-bold text-green-700">{title}</h3>
+                <p className="text-sm">{content}</p>
+                <a href={link}><span><SiGithub size={30} /></span></a>
+            </div>
+        </div>
+        
         )
  :
-            <div className=" w-full flex flex-row justify-between p-2">
-                <div className="w-[50%] flex justify-center flex-col gap-3">
-                    <h3 className="font-bold">{title}</h3>
-                    <p className="text-sm">{content}</p>
-                    <a  href={link}><span><SiGithub size={30} /></span></a>
-                </div>
-                <div className="w-[35rem] relative">
-                    <img src={image} alt="" className="object-cover" />
-                </div>
-            </div>
+ <div className="w-full flex flex-col md:flex-row gap-4 justify-between p-2">
+ <div className="w-full md:w-[50%] flex justify-center flex-col gap-3 order-2 md:order-1">
+     <h3 className="font-bold text-green-700">{title}</h3>
+     <p className="text-sm">{content}</p>
+     <a href={link}><span><SiGithub size={30} /></span></a>
+ </div>
+ <div className="w-full md:w-[35rem] order-1 md:order-2">
+     <img src={image} alt="" className="object-cover w-full" />
+ </div>
+</div>
+
     )
 }
