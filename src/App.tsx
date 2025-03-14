79934c325dfd3add967c6751d7e584ec89f11086
Hora-keven <kevensantos993@gmail.com>
2025-03-14 14:42:48 -0300

    project finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ReactTyped } from "react-typed"
 import Button from "./Components/Button"
-import kevenPhoto from './assets/keven.jfif'
+import kevenPhoto from './assets/keven1.png'
 import javaPhoto from './assets/java_photo.png'
 import djangoPhoto from './assets/django.png'
 import grpcPhoto from './assets/grpc_python.png'
@@ -19,7 +19,7 @@ import { LiaGithub, LiaLinkedin } from "react-icons/lia"
 import { useEffect, useRef, useState } from "react"
 import { CgClose } from "react-icons/cg"
 import { HiOutlineMail } from "react-icons/hi"
-
+import Logo from './assets/logo.svg'
 
 function App() {
 
@@ -57,7 +57,7 @@ function App() {
     <div className="w-full h-auto bg-[#090A11] flex flex-col items-center">
       <div className={`fixed w-full h-16 flex flex-row items justify-center z-[9999] items-center  top-0 p-2 ${bgColor} duration-300`}>
         <div className="w-[calc(100%-4rem)] flex flex-row justify-between items-center text-white">
-          <h1 className="text-2xl text-[#00df9a] font-work">Keven Santos</h1>
+          <img src={Logo}/>
           <nav
             className="hidden md:flex w-[45%] justify-between flex-row items-center"
           >
@@ -90,31 +90,36 @@ function App() {
       </div>
       <div id="about-me" className="w-full flex flex-col md:flex-row mt-28 items-center justify-center h-auto md:h-[70%]">
 
-        <div className="w-[84%] md:w-[29%] relative overflow-hidden z-20">
+        <div className="w-[84%] md:w-[57%] relative overflow-hidden z-20">
           <img
             src={kevenPhoto}
             alt="Styled"
             className="w-full h-auto object-cover"
           />
-          {/* <div className="absolute inset-0 bg-gradient-to-l from-black via-transparent to-transparent opacity-90"></div>
-          <div className="absolute inset-0 bg-gradient-to-r from-black via-transparent to-transparent opacity-90"></div>
-          <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-85"></div>
-          <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-transparent opacity-85"></div> */}
+          <div className="md:absolute md:inset-0 md:bg-gradient-to-l md:from-[#090A11]/95 md:via-[#090A11]/50 to-transparent"></div>
         </div>
 
-        <div className="text-white flex flex-col justify-center h-auto md:h-[89%] md:ml-[7rem] font-sans p-7">
-          <h1 className="text-2xl">Hi,</h1>
-          <ReactTyped className="text-2xl font-bold text-[#00df9a]" strings={['I am Keven Santos']} typeSpeed={120} backSpeed={200} loop />
-          <h1 className="text-2xl">Backend Developer</h1>
-          <p className="mt-3">I have a degree in technical systems development from SENAI.</p>
-          <p >I'm in the 3rd semester of the computer science degree course at Unimetrocamp Wyden.</p>
+        <div className="text-white flex flex-col justify-center h-[28.5rem] font-work p-7 md:relative z-20 right-16">
+          <div className="w-full flex flex-row gap-2">
+            <h1 className="md:text-[48px] text-[32px]">Hi,</h1>
+            <ReactTyped
+              className="md:text-[48px] text-[32px] font-bold text-[#00df9a]"
+              strings={['I am Keven Santos']}
+              typeSpeed={120}
+              backSpeed={200}
+              loop
+            />
+          </div>
+          <h1 className="text-2xl text-white/80">Backend Developer</h1>
+          <p className="mt-7 text-white/70 text-[18px]">I have a degree in technical systems development from SENAI.</p>
+          <p className="text-white/70 text-[18px]">I'm in the 3rd semester of the computer science degree course at Unimetrocamp Wyden.</p>
         </div>
 
 
       </div>
 
       <div id="projects" className="w-full flex gap-3 items-center justify-center flex-col mt-10">
-      <h2 className="text-[48px] font-bold font-work">PROJECTS</h2>
+        <h2 className="text-[48px] font-bold font-work">PROJECTS</h2>
         <div className="w-full flex flex-col gap-4 p-9">
 
           <ProjectsCard
@@ -159,60 +164,60 @@ function App() {
       </div>
 
       <div id="technical-skills" className="w-full flex flex-col items-center p-6 md:p-[100px] justify-center mt-10">
-  <h2 className="text-3xl md:text-[48px] font-bold font-work text-center">TECHNICAL SKILLS</h2>
-  
-  <div className="w-full max-w-5xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-12">
-    
+        <h2 className="text-3xl md:text-[48px] font-bold font-work text-center">TECHNICAL SKILLS</h2>
 
-    <div className="hover:bg-[#082421] duration-300 border-2 p-5 gap-4 rounded-xl hover:border-[#00df9a] h-32 flex flex-col items-center">
-      <h2 className="text-center font-semibold">Programming Languages</h2>
-      <div className="flex flex-wrap justify-center gap-4">
-        <PiFileCSharp size={40} />
-        <FaJava size={40} />
-        <FaPython size={40} />
+        <div className="w-full max-w-5xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-12">
+
+
+          <div className="hover:bg-[#082421] duration-300 border-2 p-5 gap-4 rounded-xl hover:border-[#00df9a] h-32 flex flex-col items-center">
+            <h2 className="text-center font-semibold">Programming Languages</h2>
+            <div className="flex flex-wrap justify-center gap-4">
+              <PiFileCSharp size={40} />
+              <FaJava size={40} />
+              <FaPython size={40} />
+            </div>
+          </div>
+
+
+          <div className="hover:bg-[#082421] duration-300 border-2 p-5 gap-4 rounded-xl hover:border-[#00df9a] h-32 flex flex-col items-center">
+            <h2 className="text-center font-semibold">Frameworks</h2>
+            <div className="flex flex-wrap justify-center gap-4">
+              <AiOutlineDotNet size={40} />
+              <SiDjango size={40} />
+              <SiFastapi size={40} />
+              <BiLogoSpringBoot size={40} />
+            </div>
+          </div>
+
+          {/* Databases */}
+          <div className="hover:bg-[#082421] duration-300 border-2 p-5 gap-4 rounded-xl hover:border-[#00df9a] h-32 flex flex-col items-center">
+            <h2 className="text-center font-semibold">Databases</h2>
+            <div className="flex flex-wrap justify-center gap-4">
+              <SiSqlite size={40} />
+              <TbSql size={40} />
+              <SiPostgresql size={40} />
+            </div>
+          </div>
+
+          {/* ORM */}
+          <div className="hover:bg-[#082421] duration-300 border-2 p-5 gap-4 rounded-xl hover:border-[#00df9a] h-32 flex flex-col items-center">
+            <h2 className="text-center font-semibold">ORM</h2>
+            <div className="flex flex-wrap justify-center gap-4">
+              <SiSqlalchemy size={40} />
+              <SiHibernate size={40} />
+            </div>
+          </div>
+
+          {/* Version Control */}
+          <div className="hover:bg-[#082421] duration-300 border-2 p-5 gap-4 rounded-xl hover:border-[#00df9a] h-32 flex flex-col items-center">
+            <h2 className="text-center font-semibold">Version Control</h2>
+            <div className="flex flex-wrap justify-center gap-4">
+              <FaGithub size={40} />
+            </div>
+          </div>
+
+        </div>
       </div>
-    </div>
-
-
-    <div className="hover:bg-[#082421] duration-300 border-2 p-5 gap-4 rounded-xl hover:border-[#00df9a] h-32 flex flex-col items-center">
-      <h2 className="text-center font-semibold">Frameworks</h2>
-      <div className="flex flex-wrap justify-center gap-4">
-        <AiOutlineDotNet size={40} />
-        <SiDjango size={40} />
-        <SiFastapi size={40} />
-        <BiLogoSpringBoot size={40} />
-      </div>
-    </div>
-
-    {/* Databases */}
-    <div className="hover:bg-[#082421] duration-300 border-2 p-5 gap-4 rounded-xl hover:border-[#00df9a] h-32 flex flex-col items-center">
-      <h2 className="text-center font-semibold">Databases</h2>
-      <div className="flex flex-wrap justify-center gap-4">
-        <SiSqlite size={40} />
-        <TbSql size={40} />
-        <SiPostgresql size={40} />
-      </div>
-    </div>
-
-    {/* ORM */}
-    <div className="hover:bg-[#082421] duration-300 border-2 p-5 gap-4 rounded-xl hover:border-[#00df9a] h-32 flex flex-col items-center">
-      <h2 className="text-center font-semibold">ORM</h2>
-      <div className="flex flex-wrap justify-center gap-4">
-        <SiSqlalchemy size={40} />
-        <SiHibernate size={40} />
-      </div>
-    </div>
-
-    {/* Version Control */}
-    <div className="hover:bg-[#082421] duration-300 border-2 p-5 gap-4 rounded-xl hover:border-[#00df9a] h-32 flex flex-col items-center">
-      <h2 className="text-center font-semibold">Version Control</h2>
-      <div className="flex flex-wrap justify-center gap-4">
-        <FaGithub size={40} />
-      </div>
-    </div>
-
-  </div>
-</div>
 
 
 
